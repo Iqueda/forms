510d5d245fa7b3e7f7f5b09e8b1265fbc9a6af47
Eduardo Rabelo <oieduardorabelo@gmail.com>
2016-08-10 23:16:27 +1200

    react #1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      nome: '',
+      email: ''
+    }
+    this.onSubmitForm = this.onSubmitForm.bind(this)
+  }
+
+  onSubmitForm(event) {
+    event.preventDefault();
+    const data = {
+      FIRST_NAME: this.state.nome,
+      EMAIL_PERSONAL: this.state.email,
+      created_at: Date.now()
+    }
+    console.log(data);
+  }
+
   render() {
+    const {nome, email} = this.state
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Formulário</h1>
+        <form id="form" onSubmit={this.onSubmitForm}>
+
+          <label htmlFor="nome">Nome</label>
+          <input
+            ref={(node) => {this.fieldName = node}}
+            onChange={() => this.setState({ nome: this.fieldName.value })}
+            value={nome}
+            type="text" name="nome" id="nome"
+          />
+
+          <label htmlFor="email">Email</label>
+          <input
+            ref={(node) => {this.fieldEmail = node}}
+            onChange={() => this.setState({ email: this.fieldEmail.value })}
+            value={email}
+            type="text" name="email" id="email"
+          />
+
+          <button type="button">Cancelar</button>
+          <button type="submit">Enviar</button>
+        </form>
       </div>
     );
   }
