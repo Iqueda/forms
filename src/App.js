ef445eb06b0ae18ab1a5fa143ce31031bc97ae6b
Eduardo Rabelo <oieduardorabelo@gmail.com>
2016-08-10 23:18:35 +1200

    react #2

--- src/App.js
+++ src/App.js
@@ -4,25 +4,20 @@ import './App.css';
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state = {
-      nome: '',
-      email: ''
-    }
     this.onSubmitForm = this.onSubmitForm.bind(this)
   }
 
   onSubmitForm(event) {
     event.preventDefault();
     const data = {
-      FIRST_NAME: this.state.nome,
-      EMAIL_PERSONAL: this.state.email,
+      FIRST_NAME: this.fieldName.value,
+      EMAIL_PERSONAL: this.fieldEmail.value,
       created_at: Date.now()
     }
     console.log(data);
   }
 
   render() {
-    const {nome, email} = this.state
     return (
       <div className="App">
         <h1>Formulário</h1>
@@ -31,17 +26,13 @@ class App extends Component {
           <label htmlFor="nome">Nome</label>
           <input
             ref={(node) => {this.fieldName = node}}
-            onChange={() => this.setState({ nome: this.fieldName.value })}
-            value={nome}
-            type="text" name="nome" id="nome"
+            type="text"
           />
 
           <label htmlFor="email">Email</label>
           <input
             ref={(node) => {this.fieldEmail = node}}
-            onChange={() => this.setState({ email: this.fieldEmail.value })}
-            value={email}
-            type="text" name="email" id="email"
+            type="text"
           />
 
           <button type="button">Cancelar</button>
